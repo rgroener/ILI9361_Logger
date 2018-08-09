02bb398ba92a96df61ac1c42078e75ac5ef41858
rgroener <username@users.noreply.github.com>
2018-08-09 12:28:05 +0000

    added circle and fill circle function

--- main.c
+++ main.c
@@ -13,6 +13,7 @@
 */
 #include <avr/pgmspace.h>
 #include <util/delay.h> /* might be <avr/delay.h>, depending on toolchain */
+#include <stdlib.h>
 #define  DFTIMG_MAIN
 
 #include "DFT48x39.h"
@@ -36,6 +37,106 @@ void LCD_Draw_Line(int x0, int y0, int x1, int y1, color_t color)
 		
 	}
 }
+
+
+void LCD_Draw_HLine( unsigned char x, unsigned char y, unsigned char w, color_t color)
+{
+	//Draw horizontal line
+	LCD_FillRect( x,y, w, 1, color );
+	
+}
+void LCD_Draw_VLine( unsigned char x, unsigned char y, unsigned char h, color_t color)
+{
+	//Draw horizontal line
+	LCD_FillRect( x,y, 1, h, color );
+	
+}
+
+
+void LCD_Draw_Fill_Circle(uint8_t x0, uint8_t y0, uint8_t r, color_t color)
+{
+	/* source for this function is:
+	 * https://github.com/andygock/glcd
+	 */
+	int8_t f = 1 - r;
+	int8_t ddF_x = 1;
+	int8_t ddF_y = -2 * r;
+	int8_t x = 0;
+	int8_t y = r;
+	
+	int16_t i;
+
+	
+	
+	for (i=y0-r; i<=y0+r; i++) {
+		LCD_SetPixel(x0, i, color);
+	}
+	
+	while (x < y) {
+		if (f >= 0) {
+			y--;
+			ddF_y += 2;
+			f += ddF_y;
+		}
+		x++;
+		ddF_x += 2;
+		f += ddF_x;
+		
+		for (i=y0-y; i<=y0+y; i++) {
+			LCD_SetPixel(x0+x, i, color);
+			LCD_SetPixel(x0-x, i, color);
+		} 
+		for (i=y0-x; i<=y0+x; i++) {
+			LCD_SetPixel(x0+y, i, color);
+			LCD_SetPixel(x0-y, i, color);
+		}    
+	}
+}
+void LCD_Draw_Circle(uint8_t x0, uint8_t y0, uint8_t r, color_t color)
+{
+	/* source for this function is:
+	 * https://github.com/andygock/glcd
+	 */
+	int8_t f = 1 - r;
+	int8_t ddF_x = 1;
+	int8_t ddF_y = -2 * r;
+	int8_t x = 0;
+	int8_t y = r;
+	
+	//glcd_update_bbox(x0-r, y0-r, x0+r, y0+r);
+	
+	LCD_SetPixel(x0, y0+r, color);
+	LCD_SetPixel(x0, y0-r, color);
+	LCD_SetPixel(x0+r, y0, color);
+	LCD_SetPixel(x0-r, y0, color);
+	
+	while (x<y) {
+		if (f >= 0) {
+			y--;
+			ddF_y += 2;
+			f += ddF_y;
+		}
+		x++;
+		ddF_x += 2;
+		f += ddF_x;
+		
+		LCD_SetPixel(x0 + x, y0 + y, color);
+		LCD_SetPixel(x0 - x, y0 + y, color);
+		LCD_SetPixel(x0 + x, y0 - y, color);
+		LCD_SetPixel(x0 - x, y0 - y, color);
+		
+		LCD_SetPixel(x0 + y, y0 + x, color);
+		LCD_SetPixel(x0 - y, y0 + x, color);
+		LCD_SetPixel(x0 + y, y0 - x, color);
+		LCD_SetPixel(x0 - y, y0 - x, color);
+		
+	}
+}
+
+
+
+
+
 uint8_t o=0;
 
 int main( void )
@@ -52,9 +153,9 @@ int main( void )
 	LCD_Puts("Green",40,10,LCD_GREEN,LCD_WHITE);
 	LCD_Puts("Blue",80,10,LCD_BLUE,LCD_WHITE);
 	LCD_ShowImage( 0,30,DFTIMG_WIDTH,DFTIMG_HEIGHT,(const unsigned char*)DFTIMG_PTR);/* ShowImage is for high/true color */
-	
-	
-	LCD_Draw_Line(100, 100, 120, 120,LCD_BLACK);
+	LCD_FillRect( 33,44, 33, 1, LCD_RED );
+	LCD_Draw_Fill_Circle(30, 90, 10,LCD_BLUE);
+	LCD_Draw_Line(100, 100, 124, 124,LCD_BLACK);
 
 	return 0;
 }
